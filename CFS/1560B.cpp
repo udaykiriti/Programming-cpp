571aa1bd07ebafe19ed0f0efa897490339d1134a
Uday__ <123718855+udaykiriti@users.noreply.github.com>
2025-08-29 11:04:19 +0530

    Refactor main function and improve code structure

--- CFS/1560B.cpp
+++ CFS/1560B.cpp
@@ -1,26 +1,20 @@
 #include <bits/stdc++.h>
 using namespace std;
 #define endl '\n';
-
 #define ll long long
 #define endl '\n'
 #define debug(n) cout << (n) << endl;
 const ll INF = 2e18 + 99;
 
-int main()
-{
+int main(int argc , char *argv[]){
   ios_base::sync_with_stdio(false);
   cin.tie(0); cout.tie(0);
-
   int t; cin >> t;
-  while (t--)
-  {
+  while (t--) {
     int a, b, c, a1, b1; cin >> a1 >> b1 >> c;
     a = max(a1, b1);
     b = min(a1, b1);
-
-    if (a - b >= b && 2 * (a - b) >= c)
-    {
+    if (a - b >= b && 2 * (a - b) >= c) {
       if (c <= a - b) c += (a - b);
       else c -= (a - b);
       cout << c << endl;
