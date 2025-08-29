33c574abb7cf822f2bd7147118e3b7221a1dc0e2
Uday__ <123718855+udaykiriti@users.noreply.github.com>
2025-08-29 12:03:39 +0530

    !!!_____!!!

--- CRT/Numbers/GCD_LCM.cpp
+++ CRT/Numbers/GCD_LCM.cpp
@@ -1,25 +1,19 @@
-#include <iostream>
+#include <bits/stdc++.h>
 using namespace std;
 
-int main()
-{
+int main(int argc , char *argv[]) {
   int a, b, gcd, lcm;
-
   cout << "Enter two numbers: ";
   cin >> a >> b;
-
   int x = a, y = b;
-  while (y != 0)
-  {
+  while (y != 0) {
     int temp = y;
     y = x % y;
     x = temp;
   }
   gcd = x;
   lcm = (a * b) / gcd;
-
   cout << "GCD: " << gcd << endl;
   cout << "LCM: " << lcm << endl;
-
   return 0;
 }
