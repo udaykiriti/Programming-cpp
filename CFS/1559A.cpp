e894ee8ce62615ad45b6817e714007f37a121d7d
Uday__ <123718855+udaykiriti@users.noreply.github.com>
2025-08-29 11:02:56 +0530

    Refactor solve function and main structure

--- CFS/1559A.cpp
+++ CFS/1559A.cpp
@@ -1,21 +1,12 @@
 #include <bits/stdc++.h>
 using namespace std;
-
 using i64 = long long;
 #define ln "\n"
 #define stop_sync ios::sync_with_stdio(false)
 #define untie_ios cin.tie(nullptr)
-#define multicase \
-  int t;          \
-  cin >> t;       \
-  while (t--)     \
-  {               \
-    solve();      \
-  }
-#define FOR(i, a, b) for (i64 i = (a); i < (b); i++) // Macro for for loop
+#define FOR(i, a, b) for (i64 i = (a); i < (b); i++)
 
-void solve()
-{
+void solve(void){
   i64 n; cin >> n;
   vector<i64> arr(n);
   FOR(i, 0, n) cin >> arr[i];
@@ -24,9 +15,9 @@ void solve()
   cout << res << ln;
 }
 
-int main()
-{
-  stop_sync;
-  untie_ios;
-  multicase return 0;
+int main(){
+  stop_sync; untie_ios;
+  int t; cin>>t;
+  while(t--) solve();
+  return 0;
 }
