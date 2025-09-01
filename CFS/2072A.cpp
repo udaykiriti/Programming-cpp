12096cd2bfa11a45cb8fb3924d2ced131057e85b
Uday__ <123718855+udaykiriti@users.noreply.github.com>
2025-09-01 21:03:49 +0530

    Refactor main function and simplify solve logic

--- CFS/2072A.cpp
+++ CFS/2072A.cpp
@@ -1,24 +1,13 @@
 #include <bits/stdc++.h>
 using namespace std;
-using i64 = long long;
-#define ln "\n"
-#define stop_sync ios::sync_with_stdio(false)
-#define untie_ios cin.tie(nullptr)
-#define GG return 0;
-
-void solve() {
-    int n, k, p; cin >> n >> k >> p;
-    if (k < (-n * p) || k > (n * p)) {
-        cout<<(-1);
-    } else {
-        int ans = abs(k);
-        cout<<((ans / p) + (ans % p ? 1 : 0));
-    }
-}
 
 int main() {
-    stop_sync; untie_ios;
-    int t; cin>>t;
-    while(t--) solve();
-    GG;
+    int t, n, k, p;
+    cin >> t;
+    while (t--) {
+        cin >> n >> k >> p;
+        if (k < -n * p || k > n * p) cout << -1 << endl;
+        else cout << (k == 0 ? 0 : (abs(k) + p - 1) / p) << endl;
+    }
+    return 0;
 }
