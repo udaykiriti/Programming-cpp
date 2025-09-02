c3614b723276fc63ccc19bb82f671b1b79c0707c
udaykiriti <udaykiriti624@gmail.com>
2025-09-02 19:53:57 +0530

    Solves test case verification problem.
    Implements a solution for verifying test cases in a grid format.
    It checks if a row or column consists entirely of the same character ('R' or 'B')
    and outputs that character if found, otherwise outputs '.'.

--- CFS/1742C.cpp
+++ CFS/1742C.cpp
@@ -1,73 +1,47 @@
 #include <bits/stdc++.h>
 using namespace std;
-
 #define ln "\n"
-#define stop_sync ios::sync_with_stdio(false)
-#define untie_ios cin.tie(nullptr)
 #define FOR(i, a, b) for (long i = (a); i < (b); i++)
-#define multicase \
-  int t;          \
-  cin >> t;       \
-  while (t--)     \
-  {               \
-    solve();      \
-  }
 
 void solve()
 {
   const int N = 8;
   vector<string> v(N);
   FOR(p, 0, N)
-  {
     cin >> v[p];
-  }
 
   char ans = '.';
-
-  FOR(row, 0, N)
-  {
+  FOR(row, 0, N){
     if (ans != '.')
       break;
     bool test = true;
-    FOR(col, 0, N)
-    {
-      if (v[row][col] != 'R')
-      {
+    FOR(col, 0, N){
+      if (v[row][col] != 'R'){
         test = false;
         break;
       }
     }
-    if (test)
-    {
-      ans = 'R';
-    }
+    if (test) ans = 'R';
   }
 
-  FOR(col, 0, N)
-  {
+  FOR(col, 0, N){
     if (ans != '.')
       break;
     bool test = true;
-    FOR(row, 0, N)
-    {
-      if (v[row][col] != 'B')
-      {
+    FOR(row, 0, N){
+      if (v[row][col] != 'B'){
         test = false;
         break;
       }
     }
-    if (test)
-    {
-      ans = 'B';
-    }
+    if (test) ans = 'B';
   }
-
   cout << ans << ln;
 }
 
-int main()
-{
-  stop_sync;
-  untie_ios;
-  multicase return 0;
+int main(){
+  ios_base::sync_with_stdio(false);
+  cin.tie(nullptr); cout.tie(nullptr);
+  int t; cin>>t;
+  while(t--) solve();
 }
