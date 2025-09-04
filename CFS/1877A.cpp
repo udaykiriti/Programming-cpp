328d75de66b6a62af979b229ce6a6a4154b82178
Uday__ <123718855+udaykiriti@users.noreply.github.com>
2025-09-04 18:42:07 +0530

    Refactor C++ code for improved readability

--- CFS/1877A.cpp
+++ CFS/1877A.cpp
@@ -1,30 +1,22 @@
 #include <bits/stdc++.h>
 using namespace std;
 #define endl '\n'
-#define EXIT return 0
+#define GG return 0
 #define FOR(i, a, b) for (int i = a; i < b; i++)
 
-void fastIO()
-{
-  ios_base::sync_with_stdio(false);
-  cin.tie(NULL); cout.tie(NULL);
-}
-
-void solve()
-{
+void solve() {
   int n, freqSum = 0; cin >> n;
-  FOR(i, 0, n - 1)
-  {
+  FOR(i, 0, n - 1) {
     int freq; cin >> freq;
     freqSum += freq;
   }
   cout << freqSum * (-1) << endl;
 }
 
-int main()
-{
-  fastIO();
+int main(int argc , char *argv[]) {
+  ios_base::sync_with_stdio(false);
+  cin.tie(NULL); cout.tie(NULL);
   int t; cin >> t;
   while (t--) solve();
-  EXIT;
+  GG;
 }
