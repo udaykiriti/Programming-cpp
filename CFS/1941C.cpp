a462edc074128d23b1ee4e375dd1a74178f1dfbd
udaykiriti <udaykiriti624@gmail.com>
2025-09-05 19:29:10 +0530

    Refactor: Remove unused competitive programming macros in 1941C.cpp

--- CFS/1941C.cpp
+++ CFS/1941C.cpp
@@ -1,42 +1,21 @@
 #include <bits/stdc++.h>
 using namespace std;
-
-#define stop_sync ios::sync_with_stdio(false)
-#define untie_ios cin.tie(nullptr)
-#define ln "\n"
-#define pb push_back
-#define sz(x) int((x).size())
-#define All(x) (x).begin(), (x).end()
-#define multicase \
-  int t;          \
-  cin >> t;       \
-  while (t--)
 #define GG return 0;
-#define PRINT(x) cout << x << ln
-#define LOOP(i, a, b) for (int i = (a); i < (b); i++)
-
 using i64 = long long;
 
 void solve()
 {
-  i64 n;
-  cin >> n;
-  string s;
-  cin >> s;
-
+  i64 n; cin >> n;
+  string s; cin >> s;
   i64 cnt = 0, idx = 0;
-
-  while (idx != string::npos)
-  {
+  while (idx != string::npos){
     idx = s.find("mapie", idx);
     if (idx == string::npos)
       break;
     ++cnt;
     s[idx + 2] = '$';
     ++idx;
-  }
-
-  idx = 0;
+  } idx = 0;
   while (idx != string::npos)
   {
     idx = s.find("map", idx);
