1cd432a9cf6c948f9002fb6409ec295b8463189a
Uday__ <123718855+udaykiriti@users.noreply.github.com>
2025-09-08 12:14:48 +0530

    Refactor solve and main functions for clarity

--- CFS/1875A.cpp
+++ CFS/1875A.cpp
@@ -1,30 +1,21 @@
-// Jellyfish and Undertale
 #include <bits/stdc++.h>
 using namespace std;
-
 #define int long long
-#define FASTIO             \
-  ios::sync_with_stdio(0); \
-  cin.tie(0);              \
-  cout.tie(0)
 #define endl '\n'
 #define FOR(i, a, b) for (int i = a; i < b; i++)
 
-void solve()
-{
+void solve(void){
   int a, b, n, x; cin>>a>>b>>n;
   int sum = b;
-  FOR(i, 0, n)
-  {
+  FOR(i, 0, n){
     cin>>x;
     sum += min(x, a - 1);
-  }
-  cout << sum << endl;
+  } cout << sum << endl;
 }
 
-int32_t main(void)
-{
-  FASTIO;
+int32_t main(int argc , char *argv[]){
+  ios_base::sync_with_stdio(false);
+  cin.tie(nullptr); cout.tie(nullptr);
   int t; cin>>t;
   while (t--) solve();
   return 0;
