756d6c90a0db172569ad9147a4b11efe34678c40
Uday__ <123718855+udaykiriti@users.noreply.github.com>
2025-09-08 12:09:25 +0530

    Update 1831A.cpp

--- CFS/1831A.cpp
+++ CFS/1831A.cpp
@@ -3,29 +3,19 @@
 #define endl '\n'
 using namespace std;
 #define FOR(i, a, b) for (int i = a; i <= b; i++)
-#define input(x) cin >> x
 
-void solve()
-{
-  ll n; input(n);
-  FOR(i, 1, n)
-  {
-    ll temp; input(temp);
+void solve(){
+  ll n; cin>>n;
+  FOR(i, 1, n) {
+    ll temp; cin>>(temp);
     cout << (n + 1) - temp << " ";
-  }
-  cout << endl;
+  }  cout << endl;
 }
 
-void FastIO()
-{
+int main(){
   ios_base::sync_with_stdio(false);
   cin.tie(0); cout.tie(0);
-}
-
-int main()
-{
-  FastIO();
-  ll T; input(T);
+  ll T; cin>>(T);
   while (T--) solve();
   return 0;
 }
