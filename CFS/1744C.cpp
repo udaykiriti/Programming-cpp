5778216bda6370c44f564d1095d73798f52b1353
Uday__ <123718855+udaykiriti@users.noreply.github.com>
2025-09-09 16:08:15 +0530

    Refactor main function and simplify macros

--- CFS/1744C.cpp
+++ CFS/1744C.cpp
@@ -1,47 +1,29 @@
 #include <bits/stdc++.h>
 using namespace std;
-
 #define ll long long
-#define FASTIO()                    \
-  ios_base::sync_with_stdio(false); \
-  cin.tie(NULL);                    \
-  cout.tie(NULL)
-#define TESTCASES() \
-  ll t;             \
-  cin >> t;         \
-  while (t--)
-#define READ(type, var) \
-  type var;             \
-  cin >> var
-#define READSTR(s) \
-  string s;        \
-  cin >> s
-#define MAX(a, b) ((a) > (b) ? (a) : (b))
-#define PRINT(x) cout << x << '\n'
 
-int main()
-{
-  FASTIO();
-  TESTCASES()
-  {
-    READ(ll, n);READ(char, c);READSTR(s);
-    s += s;
-    vector<ll> g_pos;
+int main() {
+    ios_base::sync_with_stdio(false);
+    cin.tie(NULL); cout.tie(NULL);
+    ll t; cin >> t;
+    while (t--) {
+        ll n; char c; string s; 
+        cin >> n >> c >> s;
+        s += s;
+        vector<ll> g_pos;
+        for (ll i = 0; i < s.size(); ++i)
+            if (s[i] == 'g')
+                g_pos.push_back(i);
+        ll ans = 0;
 
-    for (ll i = 0; i < s.size(); ++i)
-      if (s[i] == 'g')
-        g_pos.push_back(i);
-    ll ans = 0;
+        for (ll i = 0; i < n; ++i)
+            if (s[i] == c) {
+                auto it = lower_bound(g_pos.begin(), g_pos.end(), i);
+                if (it != g_pos.end())
+                    ans = max(ans, *it - i);
+            }
 
-    for (ll i = 0; i < n; ++i)
-    {
-      if (s[i] == c)
-      {
-        auto it = lower_bound(g_pos.begin(), g_pos.end(), i);
-        if (it != g_pos.end()) ans = MAX(ans, *it - i);
-      }
+        cout << ans << '\n';
     }
-    PRINT(ans);
-  }
-  return 0;
+    return 0;
 }
