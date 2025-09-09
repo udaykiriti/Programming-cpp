ff152fbe09760683e1920c34b802bd38d7a69c4b
Uday__ <123718855+udaykiriti@users.noreply.github.com>
2025-09-09 11:14:16 +0530

    Refactor main function and remove solve function

--- CFS/1974A.cpp
+++ CFS/1974A.cpp
@@ -1,22 +1,15 @@
 #include <bits/stdc++.h>
 using namespace std;
-#define ln "\n"
 
-void solve()
-{
-  long x, y; cin>>x>>y;
-  long screens = (y + 1) / 2;
-  x -= (15 * screens - 4 * y);
-  x = max(x, 0L);
-  screens += (x + 14) / 15;
-  cout<<(screens);
-}
-
-int main()
-{
-  ios_base::sync_with_stdio(false);
-  cin.tie(NULL); cout.tie(NULL);
-  int t; cin>>t;
-  while(t--) solve();
-  return 0;
+int main() {
+    ios::sync_with_stdio(false);
+    cin.tie(nullptr);
+    int t; cin >> t;
+    while (t--) {
+        int x, y; cin >> x >> y;
+        int ans = y / 2 + (y % 2);
+        int r = 15 * ans - 4 * y;
+        int v = (x > r) ? (x - r + 14) / 15 : 0;
+        cout << ans + v << "\n";
+    } return 0;
 }
