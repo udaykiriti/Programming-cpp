75204cb0aa8e9be60bc0592b8ddf4b1c1e84ca7d
Uday__ <123718855+udaykiriti@users.noreply.github.com>
2025-09-09 11:16:26 +0530

    Simplify main function and improve input handling

--- CFS/2051B.cpp
+++ CFS/2051B.cpp
@@ -1,6 +1,7 @@
 #include <bits/stdc++.h>
 using namespace std;
 #define ln "\n"
+<<<<<<< HEAD
 
 const int MOD = 1e9 + 7;
 int main(void){
@@ -26,4 +27,33 @@ int main(void){
         }
         cout << ans << ln;
     } return 0;
+=======
+#define pb push_back
+#define sz(x) int((x).size())
+#define All(x) (x).begin(), (x).end()
+
+int main(void) {
+  ios_base::sync_with_stdio(false); 
+  cin.tie(nullptr); cout.tie(nullptr);
+  int t; cin >> t;
+  while (t--) {
+    int n, a, b, c; cin >> n >> a >> b >> c;
+    int ans = 3 * (n / (a + b + c));
+    n %= (a + b + c);
+    if (n > 0) {
+      ++ans;
+      n -= a;
+    }
+    if (n > 0) {
+      ++ans;
+      n -= b;
+    }
+    if (n > 0) {
+      ++ans;
+      n -= c;
+    }
+    cout << ans << ln;
+  }
+  return 0;
+>>>>>>> 45f19de (Simplify main function and improve input handling)
 }
