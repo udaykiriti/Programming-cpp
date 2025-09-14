27733700ea5dcb629f756648a75c631b1b173841
Uday__ <123718855+udaykiriti@users.noreply.github.com>
2025-09-14 22:38:11 +0530

    Refactor solve and main functions for clarity

--- CFS/1914A.cpp
+++ CFS/1914A.cpp
@@ -2,26 +2,21 @@
 using namespace std;
 #define endl '\n'
 
-void solve()
-{
+void solve(){
   int n; cin >> n;
   string log; cin >> log;
   map<char, int> time_spent;
   for (char ch : log) time_spent[ch]++;
   int solved = 0;
-  for (auto [ch, time] : time_spent)
-  {
+  for (auto [ch, time] : time_spent){
     int required = ch - 'A' + 1;
     if (time >= required)
-    {
       solved++;
-    }
   }
   cout << solved << endl;
 }
 
-int main(void)
-{
+int main(void){
   ios::sync_with_stdio(false);
   cin.tie(nullptr); cout.tie(NULL);
   int t; cin >> t;
