68fe667da325f0195e4dbd8d27217a609aa6c132
Uday__ <123718855+udaykiriti@users.noreply.github.com>
2025-09-14 18:13:47 +0530

    Refactor CFS/1633A.cpp for improved readability

--- CFS/1633A.cpp
+++ CFS/1633A.cpp
@@ -1,35 +1,22 @@
 #include <bits/stdc++.h>
 using namespace std;
 #define ll long long
-#define pb push_back
-#define ff first
-#define ss second
-#define mp make_pair
-#define BYE return 0
-#define FastIO                  \
-  ios_base::sync_with_stdio(0); \
-  cin.tie(0);                   \
-  cout.tie(0)
 #define FOR(i, a, b) for (int i = a; i < b; i++)
 
-int solve(int n)
-{
+int solve(int n){
   if (n % 7 == 0) return n;
-  FOR(i, 0, 10)
-  {
+  FOR(i, 0, 10){
     int x = ((n / 10) * 10) + i;
     if (x % 7 == 0) return x;
-  }
-  return -1;
+  }return -1;
 }
 
-int main()
-{
-  FastIO; int t;cin >> t;
-  while (t--)
-  {
+int main(int argc , char *argv[]){
+  ios_base::sync_with_stdio(false);
+  cin.tie(nullptr); cout.tie(nullptr);
+  int t;cin >> t;
+  while (t--){
     int n; cin >> n;
     cout << solve(n) << endl;
-  }
-  BYE;
+  } return 0;
 }
