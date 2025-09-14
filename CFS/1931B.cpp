a049c103f7f5566f3e5f0127b7e404b927613e88
Uday__ <123718855+udaykiriti@users.noreply.github.com>
2025-09-14 22:26:14 +0530

    Refactor main function and add solve function

--- CFS/1931B.cpp
+++ CFS/1931B.cpp
@@ -1,47 +1,32 @@
 #include <bits/stdc++.h>
-#define fio                     \
-  ios_base::sync_with_stdio(0); \
-  cin.tie(0);                   \
-  cout.tie(0);
-#define debug(_) cout << #_ << " is " << _ << '\n';
-
 using namespace std;
-using ll = long long;
-using ld = long double;
-const ll mod = 1e9 + 7;
-const ll N = 2e5 + 10;
-const ll inf = 1e9;
-const ll linf = 1e18;
+#define ll long long
 
-int main()
-{
-  fio;
-  int t; cin >> t;
-  while (t--)
-  {
+void solve(){
     ll n; cin >> n;
     vector<ll> v(n);
     ll sum = 0;
-    for (auto &i : v)
-    {
+    for (auto &i : v){
       cin >> i;
       sum += i;
     }
     ll avg = sum / n;
-    for (int i = n - 1; i > 0; i--)
-    {
-      if (v[i] < avg)
-      {
+    for (int i = n - 1; i > 0; i--){
+      if (v[i] < avg) {
         v[i - 1] -= (avg - v[i]);
         v[i] = avg;
       }
     }
     bool ok = true;
     for (int i = 0; i < n; i++)
-    {
       ok &= (v[i] == avg);
-    }
     cout << (ok ? "YES" : "NO") << '\n';
-  }
-  return 0;
+    
+}
+int main(void){
+    ios_base::sync_with_stdio(false);
+    cin.tie(nullptr); cout.tie(nullptr);
+    int t; cin>>t;
+    while(t--) solve();
+    return 0;
 }
