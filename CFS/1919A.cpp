3da7a4d26fe5aa4177df10c17cd6ccc495679a06
Uday__ <123718855+udaykiriti@users.noreply.github.com>
2025-09-14 15:24:27 +0530

    Refactor main and solve functions in 1919A.cpp

--- CFS/1919A.cpp
+++ CFS/1919A.cpp
@@ -1,20 +1,17 @@
 #include <bits/stdc++.h>
 #define ll long long int
-#define nline '\n'
 using namespace std;
 
-void solve()
-{
+void solve() {
   ll a, b; cin >> a >> b;
   ll sum = a + b;
   if (sum & 1) cout << "Alice\n";
   else cout << "Bob\n";
 }
 
-int main()
-{
+int main(int argc , char *argv[]) {
   ios_base::sync_with_stdio(false);
   cin.tie(NULL); cout.tie(NULL);
-  ll t = 1; cin >> t;
+  ll t(1); cin >> t;
   while (t--) solve();
 }
