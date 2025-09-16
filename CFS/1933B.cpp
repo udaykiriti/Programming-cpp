0a77670a186376b7ab2f0cceb4e375fb31866f5a
Uday__ <123718855+udaykiriti@users.noreply.github.com>
2025-09-16 10:31:01 +0530

    Refactor main function structure and formatting

--- CFS/1933B.cpp
+++ CFS/1933B.cpp
@@ -3,17 +3,14 @@ using namespace std;
 #define ll long long
 #define FOR(i, a, b) for (ll i = a; i < b; i++)
 
-int main()
-{
+int main(void){
   ll t; cin>>t;
-  while (t--)
-  {
+  while (t--) {
     ll n; cin>>n;
     bool one = false;
     int s = 0;
 
-    FOR(p, 0, n)
-    {
+    FOR(p, 0, n) {
       int x; cin>>x;
       if (x % 3 == 1)
         one = true;
@@ -27,6 +24,5 @@ int main()
       cout<<1<<endl;
     else
       cout<<0<<endl;
-  }
-  return 0;
+  } return 0;
 }
