4c0531683818a1be3da720bf054f86eed48d6268
Uday__ <123718855+udaykiriti@users.noreply.github.com>
2025-09-16 10:41:53 +0530

    Refactor solve function and main for clarity

--- CFS/2051A.cpp
+++ CFS/2051A.cpp
@@ -4,33 +4,26 @@
 #define FOR(i, n) for (int i = 0; i < (n); ++i)
 using namespace std;
 
-void solve()
-{
+void solve(){
   int n; cin >> n;
   vi a(n), b(n);
   FOR(i, n) cin >> a[i];
   FOR(i, n) cin >> b[i];
 
   int total_m = 0, total_s = 0;
-  FOR(i, n)
-  {
+  FOR(i, n) {
     int gain = a[i];
     int penalty = (i + 1 < n) ? b[i + 1] : 0;
-    if (gain > penalty)
-    {
+    if (gain > penalty){
       total_m += a[i];
-      if (i + 1 < n)
-        total_s += b[i + 1];
+      if (i + 1 < n) total_s += b[i + 1];
     }
-  }
-  cout << total_m - total_s << endl;
+  } cout << total_m - total_s << endl;
 }
 
-int32_t main()
-{
+int32_t main(int argc , char *argv[]){
   ios_base::sync_with_stdio(false);
   cin.tie(NULL); cout.tie(NULL);
   int t; cin >> t;
   while (t--) solve();
-  return 0;
 }
