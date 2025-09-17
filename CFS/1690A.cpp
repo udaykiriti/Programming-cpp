1897468ee4622b1793feb2fccea1f1345e8fdbe2
Uday__ <123718855+udaykiriti@users.noreply.github.com>
2025-09-17 16:12:35 +0530

    Refactor main function to use C++ streams

--- CFS/1690A.cpp
+++ CFS/1690A.cpp
@@ -1,31 +1,24 @@
-#include <cstdio>
+#include <bits/stdc++.h>
+using namespace std;
 
-int main()
-{
-  long t; scanf("%ld", &t);
-  while (t--)
-  {
-    long n;
-    scanf("%ld", &n);
-    long a, b, c;
-    if (n % 3 == 0)
-    {
-      a = b = c = (n - 3) / 3;
-      a += 1;
-      b += 2;
+int main() {
+    ios::sync_with_stdio(false);
+    cin.tie(nullptr);
+
+    int t; cin >> t;
+    while (t--) {
+        long n; cin >> n;
+        long a, b, c;
+        if (n % 3 == 0) {
+            a = b = c = (n - 3) / 3;
+            a += 1; b += 2;
+        } else if (n % 3 == 1) {
+            a = b = c = (n - 4) / 3;
+            a += 1; b += 3;
+        } else {
+            a = b = c = (n - 5) / 3;
+            a += 2; b += 3;
+        }
+        cout << a << " " << b << " " << c << "\n";
     }
-    else if (n % 3 == 1)
-    {
-      a = b = c = (n - 4) / 3;
-      a += 1;
-      b += 3;
-    }
-    else if (n % 3 == 2)
-    {
-      a = b = c = (n - 5) / 3;
-      a += 2;
-      b += 3;
-    }
-    printf("%ld %ld %ld\n", a, b, c);
-  }
 }
