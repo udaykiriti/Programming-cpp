77d82baaf63867dac1cc44127c12e500c5cbca05
Uday__ <123718855+udaykiriti@users.noreply.github.com>
2025-09-18 11:19:15 +0530

    Refactor main function and clean up code

--- CFS/1462C.cpp
+++ CFS/1462C.cpp
@@ -1,52 +1,35 @@
 #include <bits/stdc++.h>
-#define ll long long
-#define ull unsigned long long
-#define pb push_back
-#define fastread() (ios_base::sync_with_stdio(false), cin.tie(NULL))
-#define PRINT(x) cout << x << endl
-#define INPUT(x) cin >> x
-#define SORT(s) sort(s.begin(), s.end())
 using namespace std;
 
-int main()
-{
-  fastread();
-  ll t, n;
-  INPUT(t);
+int main() {
+    ios_base::sync_with_stdio(false);
+    cin.tie(NULL); cout.tie(nullptr);
+    long long t; cin >> t;
+    while (t--) {
+        long long n; cin >> n;
+        if (n < 10) {
+            cout << n << "\n";
+            continue;
+        }
 
-  while (t--)
-  {
-    string s;
-    ll x = 9, sum = 0;
-    set<int> S;
-    INPUT(n);
+        string s;
+        long long x = 9, sum = 0;
+        set<int> digits;
 
-    if (n < 10)
-    {
-      PRINT(n);
-      continue;
+        while (x > 0) {
+            if (n - sum >= x) {
+                sum += x;
+                s.push_back(char(x + '0'));
+                digits.insert(x);
+            }
+            x--;
+        }
+        if ((long long)s.length() != (long long)digits.size() || sum != n) {
+            cout << -1 << "\n";
+        } else {
+            sort(s.begin(), s.end());
+            cout << s << "\n";
+        }
     }
-
-    while (x > 0)
-    {
-      if (n - sum >= x)
-      {
-        sum += x;
-        s += (x + '0');
-        S.insert(x);
-      }
-      x--;
-    }
-
-    if (s.length() != S.size() || sum != n)
-    {
-      PRINT(-1);
-    }
-    else
-    {
-      SORT(s);
-      PRINT(s);
-    }
-  }
-  return 0;
+    return 0;
 }
