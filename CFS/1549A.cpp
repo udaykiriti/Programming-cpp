aea6c30ea088912e179d383d531ee6d647b032da
Uday__ <123718855+udaykiriti@users.noreply.github.com>
2025-09-20 17:13:23 +0530

    Refactor main function and streamline input/output

--- CFS/1549A.cpp
+++ CFS/1549A.cpp
@@ -1,27 +1,15 @@
 #include <bits/stdc++.h>
 using namespace std;
 
-#define ll long long
-#define endl '\n'
-#define debug(n) cout << (n) << endl;
-const ll INF = 2e18 + 99;
-#define FastIO             \
-  ios::sync_with_stdio(0); \
-  cin.tie(0);              \
-  cout.tie(0);
-#define BYE return 0;
-#define Scan(x) cin >> x
-int main()
-{
+int main() {
+    ios::sync_with_stdio(false);
+    cin.tie(nullptr);
 
-  FastIO int t;
-  Scan(t);
-  int n;
-  while (t--)
-  {
-    Scan(n);
-    n--;
-    cout << 2 << " " << n << endl;
-  }
-  BYE
+    int t; cin >> t;
+    while (t--) {
+        int n;cin >> n;
+        n--;
+        cout << 2 << " " << n << '\n';
+    }
+    return 0;
 }
