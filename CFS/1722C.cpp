28a737a632c60d6b66cc8def67df7b989f110304
Uday__ <123718855+udaykiriti@users.noreply.github.com>
2025-09-22 11:27:49 +0530

    1722C

--- CFS/1722C.cpp
+++ CFS/1722C.cpp
@@ -1,56 +1,48 @@
 #include <bits/stdc++.h>
 using namespace std;
-using i64 = long long int;
+using i64 = long long;
 #define ln "\n"
 #define stop_sync ios::sync_with_stdio(false)
 #define untie_ios cin.tie(nullptr)
-#define pb push_back
-#define All(x) (x).begin(), (x).end()
 
-void solve()
-{
-  const int N = 3;
-  i64 t; cin >> t;
+void solve() {
+    const int N = 3;
+    i64 t; cin >> t;
 
-  while (t--)
-  {
-    i64 n; cin >> n;
-    vector<set<string>> v(N);
-    for (int p = 0; p < N; p++)
-    {
-      for (int q = 0; q < n; q++)
-      {
-        string s; cin >> s;
-        v[p].insert(s);
-      }
-    }
+    while (t--) {
+        i64 n; cin >> n;
 
-    vector<i64> score(N, 0);
-    for (int p = 0; p < N; p++)
-    {
-      for (const auto &x : v[p])
-      {
-        i64 cnt = 0;
-        for (int q = 0; q < N; q++)
-        {
-          if (q == p) continue;
-          cnt += v[q].count(x);
+        vector<set<string>> v(N);
+        for (int p = 0; p < N; p++) {
+            for (int q = 0; q < n; q++) {
+                string s; cin >> s;
+                v[p].insert(s);
+            }
         }
 
-        if (cnt == 0) score[p] += 3;
-        else if (cnt == 1) score[p] += 1;
-      }
-    }
+        vector<i64> score(N, 0);
+        for (int p = 0; p < N; p++) {
+            for (const auto &x : v[p]) {
+                i64 cnt = 0;
+                for (int q = 0; q < N; q++) {
+                    if (q == p) continue;
+                    cnt += v[q].count(x);
+                }
 
-    for (int p = 0; p < N; p++) cout << score[p] << " ";
-    cout << ln;
-  }
+                if (cnt == 0) score[p] += 3;
+                else if (cnt == 1) score[p] += 1;
+            }
+        }
+
+        for (int p = 0; p < N; p++) {
+            cout << score[p] << " ";
+        }
+        cout << ln;
+    }
 }
 
-int main()
-{
-  stop_sync;
-  untie_ios;
-  solve();
-  return 0;
+int main(void) {
+    stop_sync; untie_ios;
+    solve();
+    return 0;
 }
