50bf210912c4618e40e28a3955eef086a56db16c
Uday__ <123718855+udaykiriti@users.noreply.github.com>
2025-09-23 14:36:28 +0530

    Refactor main function and improve formatting

--- CFS/1520C.cpp
+++ CFS/1520C.cpp
@@ -1,21 +1,19 @@
 #include <bits/stdc++.h>
 using namespace std;
-#define FastIO ios::sync_with_stdio(0), cin.tie(0), cout.tie(0)
 #define endl '\n'
 
-int main()
-{
-  FastIO; int t; cin >> t;
-  while (t--)
-  {
+int main(){
+  ios::sync_with_stdio(0); cin.tie(0); cout.tie(0); 
+  int t; cin >> t;
+  while (t--){
     int n; cin >> n;
-    if (n == 2)
+    if (n == 2){
       cout << -1 << endl;
-    else
-    {
+    }
+    else {
       for (int i = 1; i <= n * n; i += 2) cout << i << ' ';
       for (int i = 2; i <= n * n; i += 2) cout << i << ' ';
       cout << endl;
     }
-  }
+  }return 0;
 }
