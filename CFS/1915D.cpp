0822ad3f43eb94f0c47bf994377e3a943a70cc1b
Uday__ <123718855+udaykiriti@users.noreply.github.com>
2025-09-24 09:19:53 +0530

    Refactor solve function and main for clarity

--- CFS/1915D.cpp
+++ CFS/1915D.cpp
@@ -1,47 +1,47 @@
 #include <bits/stdc++.h>
 using namespace std;
-const char nl = '\n';
-#define ll long long
-#define FAST_IO                     \
-  ios_base::sync_with_stdio(false); \
-  cin.tie(NULL);                    \
-  cout.tie(NULL)
-#define isVowel(c) ((c) == 'a' || (c) == 'e')
-#define REVERSE(s) reverse(s.begin(), s.end())
-#define FOR(i, a, b) for (int i = a; i < b; ++i)
 
-void solve()
-{
-  ll n; string s, ans = ""; cin >> n >> s;
-  REVERSE(s);
-  FOR(i, 0, n)
-  {
-    if (!isVowel(s[i]) && isVowel(s[i + 1]) && !isVowel(s[i + 2]))
-    {
-      ans += s[i];
-      ans += s[i + 1];
-      ans += s[i + 2];
-      ans += '.';
-      i += 2;
-    }
-    else
-    {
-      ans += s[i];
-      ans += s[i + 1];
-      ans += '.';
-      i += 1;
-    }
-  }
+using ll = long long;
 
-  REVERSE(ans);
-  ans.erase(ans.begin());
-  cout << ans << nl;
+void solve() {
+    ll n;
+    string s, ans = "";
+    cin >> n >> s;
+
+    reverse(s.begin(), s.end());
+
+    for (int i = 0; i < n; i++) {
+        auto isVowel = [](char c) {
+            return c == 'a' || c == 'e';
+        };
+
+        if (i + 2 < n && !isVowel(s[i]) && isVowel(s[i + 1]) && !isVowel(s[i + 2])) {
+            ans += s[i];
+            ans += s[i + 1];
+            ans += s[i + 2];
+            ans += '.';
+            i += 2;
+        } else if (i + 1 < n) {
+            ans += s[i];
+            ans += s[i + 1];
+            ans += '.';
+            i += 1;
+        } else {
+            ans += s[i]; 
+        }
+    }
+
+    reverse(ans.begin(), ans.end());
+    if (!ans.empty() && ans.front() == '.')
+        ans.erase(ans.begin());
+
+    cout << ans << '\n';
 }
 
-int main()
-{
-  FAST_IO;
-  int T = 1; cin >> T;
-  while (T--) solve();
-  return 0;
+int main() {
+    ios::sync_with_stdio(false);
+    cin.tie(nullptr);
+    int T(1); cin >> T;
+    while (T--) solve();
+    return 0;
 }
