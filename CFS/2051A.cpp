d0a64346c5471242edbf64ccb3eb9bd0945d3312
Uday__ <123718855+udaykiriti@users.noreply.github.com>
2025-09-25 11:58:15 +0530

    Refactor FOR macro for custom range support
    Updated FOR macro to allow custom range and improved loop syntax.

--- CFS/2051A.cpp
+++ CFS/2051A.cpp
@@ -1,17 +1,17 @@
 #include <bits/stdc++.h>
 #define int long long
 #define vi vector<int>
-#define FOR(i, n) for (int i = 0; i < (n); ++i)
+#define FOR(i,a,b) for (int i = a; i < (b); ++i)
 using namespace std;
 
 void solve(){
   int n; cin >> n;
   vi a(n), b(n);
-  FOR(i, n) cin >> a[i];
-  FOR(i, n) cin >> b[i];
+  FOR(i,0,n) cin >> a[i];
+  FOR(i,0,n) cin >> b[i];
 
   int total_m = 0, total_s = 0;
-  FOR(i, n) {
+  FOR(i, 0,n) {
     int gain = a[i];
     int penalty = (i + 1 < n) ? b[i + 1] : 0;
     if (gain > penalty){
