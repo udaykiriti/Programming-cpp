dd43ef0aa0ae7cf17599573c8bb9bf9be9dd7916
Uday__ <123718855+udaykiriti@users.noreply.github.com>
2025-09-30 16:04:01 +0530

    Update 1538B.cpp

--- CFS/1538B.cpp
+++ CFS/1538B.cpp
@@ -1,55 +1,33 @@
 #include <bits/stdc++.h>
 using namespace std;
 
-#define FastIO                      \
-  ios_base::sync_with_stdio(false); \
-  cin.tie(0);                       \
-  cout.tie(0);
-
-#define FOR(i, start, end) for (ll i = start; i <= end; ++i)
-
 using ll = long long int;
-
 ll aarray[1000005];
 
-void solve()
-{
-  ll n, sum = 0, ans = 0;
-  cin >> n;
-
-  FOR(i, 1, n)
-  {
-    ll x;
-    cin >> x;
-    aarray[i] = x;
-    sum += x;
-  }
-
-  if (sum % n != 0)
-  {
-    cout << -1 << "\n";
-    return;
-  }
-
-  ll eq = sum / n;
-
-  FOR(i, 1, n)
-  {
-    if (aarray[i] > eq)
-      ans++;
-  }
-
-  cout << ans << "\n";
+void solve() {
+    ll n, sum = 0, ans = 0;
+    cin >> n;
+    for (ll i = 1; i <= n; i++) {
+        ll x; cin >> x;
+        aarray[i] = x;
+        sum += x;
+    }
+    if (sum % n != 0) {
+        cout << -1 << "\n";
+        return;
+    }
+    ll eq = sum / n;
+    for (ll i = 1; i <= n; i++) {
+        if (aarray[i] > eq)
+            ans++;
+    }
+    cout << ans << "\n";
 }
 
-int main()
-{
-  FastIO
-      ll t;
-  cin >> t;
-
-  while (t--)
-    solve();
-
-  return 0;
+int main() {
+    ios_base::sync_with_stdio(false);
+    cin.tie(0); cout.tie(0);
+    ll t; cin >> t;
+    while (t--) solve();
+    return 0;
 }
