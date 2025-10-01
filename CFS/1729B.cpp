1d3c82c61c19e3902ccb8484dcce3ae755665ab2
Uday__ <123718855+udaykiriti@users.noreply.github.com>
2025-10-01 08:14:16 +0530

    Refactor solve function and main for clarity

--- CFS/1729B.cpp
+++ CFS/1729B.cpp
@@ -1,38 +1,28 @@
 #include <bits/stdc++.h>
 using namespace std;
-#define input(x) cin >> x
-#define output(x) cout << x << '\n'
-#define FAST_IO()                   \
-  ios_base::sync_with_stdio(false); \
-  cin.tie(nullptr);
 
-void solve()
-{
-  int n; input(n);
-  string s;input(s);
-  string ans;
-  for (int i = n - 1; i >= 0;)
-  {
-    if (s[i] == '0')
-    {
-      int num = stoi(s.substr(i - 2, 2));
-      ans += char(96 + num);
-      i -= 3;
+void solve() {
+    int n; cin >> n;
+    string s; cin >> s;
+    string ans;
+    for (int i = n - 1; i >= 0;) {
+        if (s[i] == '0') {
+            int num = stoi(s.substr(i - 2, 2));
+            ans += char(96 + num);
+            i -= 3;
+        } else {
+            ans += char(96 + (s[i] - '0'));
+            i--;
+        }
     }
-    else
-    {
-      ans += char(96 + (s[i] - '0'));
-      i--;
-    }
-  }
-  reverse(ans.begin(), ans.end());
-  output(ans);
+    reverse(ans.begin(), ans.end());
+    cout << ans << "\n";
 }
 
-int main()
-{
-  FAST_IO();
-  int t; input(t);
-  while (t--) solve();
-  return 0;
+int main() {
+    ios_base::sync_with_stdio(false);
+    cin.tie(nullptr); cout.tie(0);
+    int t; cin >> t;
+    while (t--) solve();
+    return 0;
 }
