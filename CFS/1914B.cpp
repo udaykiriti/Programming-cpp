7a7b3454e051842db3c650398f9ed3842d48fe7a
Uday__ <123718855+udaykiriti@users.noreply.github.com>
2025-10-02 07:37:26 +0530

    Refactor input/output settings in main function
    Replaced fastio macro with direct ios_base settings for better clarity.

--- CFS/1914B.cpp
+++ CFS/1914B.cpp
@@ -1,12 +1,5 @@
 #include <bits/stdc++.h>
 using namespace std;
-#define fastio                 \
-  ios::sync_with_stdio(false); \
-  cin.tie(NULL);
-#define testcases \
-  int t;          \
-  cin >> t;       \
-  while (t--)
 #define FOR(i, a, b) for (long i = (a); i <= (b); ++i)
 #define RFOR(i, a, b) for (long i = (a); i > (b); --i)
 #define endl '\n'
@@ -22,7 +15,8 @@ void solve(){
 
 int main()
 {
-  fastio;
+  ios_base::sync_with_stdio(false);
+  cin.tie(0); cout.tie(0);
   int t; cin>>t;
   while(t--) solve();
   return 0;
