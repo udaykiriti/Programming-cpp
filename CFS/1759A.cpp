e7f5e1f52fb87907a33b549099a7be23632d62fe
Uday__ <123718855+udaykiriti@users.noreply.github.com>
2025-10-02 11:08:47 +0530

    Refactor repeated string construction in solve function

--- CFS/1759A.cpp
+++ CFS/1759A.cpp
@@ -4,15 +4,14 @@ using namespace std;
 
 void solve()
 {
-  string repeated_yes = "";
-  for (int i = 0; i < 100; i++) repeated_yes += "Yes";
+  string res = "";
+  for (int i = 0; i < 100; i++) res += "Yes";
   string s; cin >> s;
-  if (repeated_yes.find(s) != string::npos) cout << "YES" << endl;
+  if (res.find(s) != string::npos) cout << "YES" << endl;
   else cout << "NO" << endl;
 }
 
-int main()
-{
+int main() {
   ios_base::sync_with_stdio(false);
   cin.tie(NULL); cout.tie(NULL);
   int t; cin >> t;
