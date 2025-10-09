4ef246b9d06fb2a7292eef4988892b430498f193
Uday__ <123718855+udaykiriti@users.noreply.github.com>
2025-10-09 08:46:12 +0530

    Enable multiple test cases in solve function

--- CFS/1709A.cpp
+++ CFS/1709A.cpp
@@ -3,9 +3,6 @@ using namespace std;
 
 void solve()
 {
-  int t; cin >> t;
-  while (t--)
-  {
     int x; cin >> x;
     vector<int> doors(4);
     for (int i = 1; i <= 3; ++i) cin >> doors[i];
@@ -13,13 +10,13 @@ void solve()
     int second = doors[first];
     if (first != 0 && second != 0 && doors[second] != 0) cout << "YES\n";
     else cout << "NO\n";
-  }
 }
 
 int main()
 {
   ios::sync_with_stdio(false);
   cin.tie(NULL); cout.tie(NULL);
-  solve();
+  int t; cin >> t;
+  while (t--) solve();
   return 0;
 }
