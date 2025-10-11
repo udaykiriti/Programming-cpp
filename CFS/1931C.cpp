a3192bb03db65cf4d2a17dab426e8b484b4acd0a
udaykiriti <udaykiriti624@gmail.com>
2025-10-11 11:44:29 +0530

    1931C

--- CFS/1931C.cpp
+++ CFS/1931C.cpp
@@ -5,23 +5,19 @@ int main() {
     long t; scanf("%ld", &t);
 
     while (t--) {
-        long n;
-        scanf("%ld", &n);
+        long n; scanf("%ld", &n);
 
         std::vector<long> a(n);
-        for (long i = 0; i < n; ++i) {
+        for (long i = 0; i < n; ++i)
             scanf("%ld", &a[i]);
-        }
 
         long left = 1;
-        while (left < n && a[left] == a[0]) {
+        while (left < n && a[left] == a[0])
             ++left;
-        }
 
         long right = 1;
-        while (right < n && a[n - 1 - right] == a.back()) {
+        while (right < n && a[n - 1 - right] == a.back())
             ++right;
-        }
 
         long s = (left > right) ? left : right;
         if (a[0] == a.back()) 
@@ -30,6 +26,5 @@ int main() {
         if (s > n) s = n;
         printf("%ld\n", n - s);
     }
-
     return 0;
 }
