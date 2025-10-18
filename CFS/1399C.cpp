729a481059a6ff81ba61bfc29b4dc661b4585198
udaykiriti <udaykiriti624@gmail.com>
2025-10-18 12:20:21 +0530

    1547A

--- CFS/1399C.cpp
+++ CFS/1399C.cpp
@@ -1,3 +1,8 @@
+/******************************
+ *    author:  uday           *
+ *    created: 18 11:56:39    *
+ ******************************/
+
 #include <bits/stdc++.h>
 using namespace std;
 #define endl '\n'
@@ -6,22 +11,19 @@ void solve()
 {
   int n; cin >> n;
   vector<int> w(n);
-  for (int i = 0; i < n; ++i)
-    cin >> w[i];
+  for (int i = 0; i < n; ++i) cin >> w[i];
 
-  int max_teams = 0;
+  int res = 0;
 
   for (int s = 2; s <= 2 * n; ++s)
   {
     vector<int> freq(n + 1, 0);
-    for (int weight : w)
-      freq[weight]++;
+    for (int weight : w) freq[weight]++;
     int teams = 0;
     for (int i = 1; i <= n; ++i)
     {
       int j = s - i;
-      if (j < 1 || j > n)
-        continue;
+      if (j < 1 || j > n) continue;
 
       if (i == j)
       {
@@ -29,22 +31,22 @@ void solve()
       }
       else
       {
-        int pair_count = min(freq[i], freq[j]);
-        teams += pair_count;
-        freq[i] -= pair_count;
-        freq[j] -= pair_count;
+        int pairs = min(freq[i], freq[j]);
+        teams += pairs;
+        freq[i] -= pairs;
+        freq[j] -= pairs;
       }
     }
-    max_teams = max(max_teams, teams);
+    res = max(res, teams);
   }
-  cout << max_teams << endl;
+  cout << res << endl;
 }
 
 int main()
 {
   ios_base::sync_with_stdio(false);
   cin.tie(nullptr); cout.tie(NULL);
-  int t; cin >> t;
-  while (t--) solve();
+  int t(1),tcase(0); cin >> t;
+  while (++tcase,t--) solve();
   return 0;
 }
