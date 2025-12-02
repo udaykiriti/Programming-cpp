5df7c56ae1b91d3710cea89ac38aaed7ebee69b9
udaykiriti <udaykiriti624@gmail.com>
2025-12-02 15:08:13 +0530

    1A

--- CFS/711A.cpp
+++ CFS/711A.cpp
@@ -1,30 +1,24 @@
 #include <bits/stdc++.h>
 using namespace std;
-#define endl '\n'
 
-int main()
-{
+int main() {
   ios_base::sync_with_stdio(false);
   cin.tie(0); cout.tie(0);
   int n; cin >> n;
   vector<string> bus(n);
   bool found = false;
 
-  for (int i = 0; i < n; ++i)
+  for (int i = 0; i < n; i++)
     cin >> bus[i];
 
-  for (int i = 0; i < n; ++i)
-  {
-    if (!found)
-    {
-      if (bus[i][0] == 'O' && bus[i][1] == 'O')
-      {
+  for (int i = 0; i < n; i++) {
+    if (!found) {
+      if (bus[i][0] == 'O' && bus[i][1] == 'O') {
         bus[i][0] = bus[i][1] = '+';
         found = true;
         break;
       }
-      if (bus[i][3] == 'O' && bus[i][4] == 'O')
-      {
+      if (bus[i][3] == 'O' && bus[i][4] == 'O') {
         bus[i][3] = bus[i][4] = '+';
         found = true;
         break;
@@ -32,15 +26,13 @@ int main()
     }
   }
 
-  if (found)
-  {
-    cout << "YES\n";
+  if (found) {
+    cout << "YES" << '\n';
     for (const string &row : bus)
       cout << row << '\n';
   }
-  else
-  {
-    cout << "NO\n";
+  else {
+    cout << "NO" << '\n';
   }
   return 0;
 }
