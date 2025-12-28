706d30cc7a7e308bb3a46758c3b34e964ba62881
udaykiriti <udaykiriti624@gmail.com>
2025-12-28 13:04:24 +0530

    modified

--- algo/Template.cpp
+++ algo/Template.cpp
@@ -507,7 +507,6 @@ int main(/* int argc, char *argv[] */) {
     ios_base::sync_with_stdio(0);
     cin.tie(0); cout.tie(0);
     //cin.tie(0)->ios::sync_with_stdio(0);
-   // cout.tie(0);
     #ifdef ONPC
         freopen("in.txt", "r", stdin); freopen("out.txt", "w", stdout);
         cout << "o_o >--< o_o >>>>>>>>>> Compiled <<<<<<<<<< o_o >--< o_o" << '\n';
