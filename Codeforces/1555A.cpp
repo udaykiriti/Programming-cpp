101c159803838bfbd2d851d3497dadfd4cfce19a
udaykiriti <udaykiriti624@gmail.com>
2026-01-09 20:54:04 +0530

    224A.cpp

--- Codeforces/1555A.cpp
+++ Codeforces/1555A.cpp
@@ -1,23 +1,15 @@
 #include<bits/stdc++.h>
 using namespace std;
-
-
 #define ll long long
-#define endl '\n'
-#define debug(n) cout<<(n)<<endl;
 const ll INF = 2e18 + 99;
 
 int main(){
   ios_base::sync_with_stdio(false);
-  cin.tie(NULL);
-
-  int t;
-  cin>>t;
+  cin.tie(nullptr); cout.tie(nullptr);
+  int t; cin>>t;
   ll n;
   while(t--){
     cin>>n;
-
-    cout<<max(6LL, n + 1) / 2 * 5 <<endl;
+    cout<<max(6LL, n + 1) / 2 * 5 << '\n';
   }
-
 }
