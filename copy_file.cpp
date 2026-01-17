572c2430cb3a81c20cf6ed7d9dd001aafd34faf2
udaykiriti <udaykiriti624@gmail.com>
2026-01-17 14:41:42 +0530

    Add I/O optimization to copy_file.cpp

--- copy_file.cpp
+++ copy_file.cpp
@@ -33,6 +33,8 @@ bool copy_file(const string& source, const string& dest) {
 }
 
 int main() {
+    ios_base::sync_with_stdio(false);
+    cin.tie(0); cout.tie(0);
     string source, dest;
     
     cout << "Enter source file path: ";
