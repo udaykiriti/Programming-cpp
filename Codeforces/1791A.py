8b8f3b9ea5665dace49f6720fc8c38d5314532e0
udaykiriti <udaykiriti624@gmail.com>
2026-01-17 09:58:02 +0530

    organized in a good way

--- Codeforces/1791A.py
+++
@@ -1,6 +0,0 @@
-for _ in range(int(input())):
-    ch = input()
-    if ch in "codeforces":
-        print("YES")
-    else:
-        print("NO")
--- Codeforces/1899A.py
+++
@@ -1,7 +0,0 @@
-def solve():
-  n=int(input())
-  print('second' if n%3==0 else 'second')
-
-t=int(input())
-for i in range(t):
-  solve()
