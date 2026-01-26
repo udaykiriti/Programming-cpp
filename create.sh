eaa74c3726ec28d6a163725c9b5b4cb1d2a48eae
udaykiriti <udaykiriti624@gmail.com>
2026-01-26 23:45:13 +0530

    Changes Script for create

--- create.sh
+++ create.sh
@@ -5,11 +5,12 @@ TEMPLATE="$HOME/Programming-cpp/algo/Template.cpp"
 RED='\033[0;31m'
 GREEN='\033[0;32m'
 YELLOW='\033[1;33m'
+ORANGE='\033[38;2;255;140;0m'
 BLUE='\033[0;34m'
 NC='\033[0m'
 
 if [ -z "$1" ]; then
-  echo -e "${YELLOW}Usage:${NC} create <name> [-cpp]"
+  echo -e "${YELLOW}[Usage] :${NC} create <name> [-cpp]"
   exit 1
 fi
 
@@ -21,18 +22,18 @@ if [ "$EXTENSION" == "-cpp" ]; then
 fi
 
 if [ ! -f "$TEMPLATE" ]; then
-  echo -e "${RED} Error:${NC} Template not found"
-  echo -e "${BLUE}Path:${NC} $TEMPLATE"
+  echo -e "${RED}[Error] :${NC} Template not found"
+  echo -e "${BLUE}[Path] :${NC} $TEMPLATE"
   exit 1
 fi
 
 if [ -f "$FILENAME" ]; then
-  echo "Please check once before Trying another time.."
-  echo -e "${YELLOW}Warning:${NC} $FILENAME Already exists.... "
-  echo "Danger [Verify Twice and Enter "y" cuz the Data will be Destroyed"
-  read -p "Overwrite? (y/n) [Now the Danger Begins]: " choice
+  echo -e "${ORANGE}[Ohhh..]: Please check once before Trying another time..${NC}"
+  echo -e "${ORANGE}[Warning] :${NC} $FILENAME Already exists....${NC} "
+  echo -e "${RED}[Danger]: Verify Twice and Enter "y" cuz the Data will be Destroyed${NC}"
+  read -rp "[R u Sure..]: Overwrite? (y/n) [Now the Danger Begins]: " choice
   [[ "$choice" != "y" ]] && echo -e "${RED}Aborted...[I mean Operation Cancelled]" && exit 1
 fi
 
 cp "$TEMPLATE" "$FILENAME" &&
-  echo -e "${GREEN} Delivered:${NC} $FILENAME from algo/Template.cpp"
+  echo -e "${GREEN}[Delivered] :${NC} $FILENAME from algo/Template.cpp"
