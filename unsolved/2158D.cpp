16fcc1c2d33ed73e30165f0dfc74d1f4117f4050
udaykiriti <udaykiriti624@gmail.com>
2026-02-17 12:17:40 +0530

    make some minimal changes and some pending tasks

--- unsolved/2158D.cpp
+++ unsolved/2158D.cpp
@@ -1,9 +1,13 @@
 #include <bits/stdc++.h>
 using namespace std;
 
+
+#define pb push_back
+
 int main() {
     ios::sync_with_stdio(false);
     cin.tie(nullptr);
+    cout.tie(nullptr);
 
     int T;
     cin >> T;
@@ -15,20 +19,20 @@ int main() {
 
         vector<int> pos;
         for (int i = 0; i < n; i++) {
-            if (s[i] != t[i]) pos.push_back(i + 1);  // 1-based
+            if (s[i] != t[i]) pos.pb(i + 1); 
         }
 
         if (pos.size() % 2 == 1) {
-            cout << -1 << "\n";
+            cout << -1 << '\n' ;
             continue;
         }
 
         vector<pair<int,int>> ops;
         for (int i = 0; i < (int)pos.size(); i += 2) {
-            ops.push_back({pos[i], pos[i+1]});
+            ops.pb({pos[i], pos[i+1]});
         }
 
-        cout << ops.size() << "\n";
+        cout << ops.size() << '\n';
         for (auto &p : ops) {
             cout << p.first << " " << p.second << "\n";
         }
